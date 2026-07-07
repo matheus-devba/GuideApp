7923670a045a3d01c32ba38f5ed0b8906ba7bd1d
Matheus <matheusasantos0501@gmail.com>
2026-07-07 16:34:31 -0300

    fix: views search.js

--- frontend/js/components/search.js
+++ frontend/js/components/search.js
@@ -28,7 +28,7 @@ export function searchRender(query) {
             <h2>${product.name}</h2>
             <span class="metrics-product-all">
                 <p class="rate">${product.rate}</p>
-                <p class="views">${product.view}</p>
+                <p class="views">${product.view} pessoas já viram</p>
             </span>
             <div class="product-footer-all">
                 <div class="price-group-all">
