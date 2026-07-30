b97259f2ae6962b2a9d3b0d1789f5dee84a81991
Matheus <matheusasantos0501@gmail.com>
2026-07-30 14:05:46 -0300

    aba home preview [merchant]

--- frontend/js/merchant/home.js
+++ frontend/js/merchant/home.js
@@ -1,10 +1,43 @@
-import { btnSearch } from '../components/shareButton.js'
-import { getLojaId } from '../services/requisicoesMerchant.js'
+import { btnShare } from '../components/shareButton.js'
+import { verificarUser, getLojaId, insertNomeDaLoja, verificacaoUsuario } from "../services/requisicoesMerchant.js";
+import { API_BASE_URL } from "../api/config.js"
+
 
 
 export async function initHome() {
-    const loja_id = await getLojaId()
-    console.log(loja_id)
-    btnSearch(`/lojas/${loja_id.id}?loja_id=${loja_id.id}`)
+  const verificar = await verificacaoUsuario();
+  if (!verificar) return; // Se for false (não logado), para a execução aqui.
+
+  const lojaId = await getLojaId()
+
+  await insertNomeDaLoja(lojaId.id)
+    btnShare(`/lojas/${lojaId.id}?lojaId.id=${lojaId.id}`,
+        "Olha o que achei no Guide",
+        "Confira a minha loja no Guide:"
+    )
+
+    const responseLoja = await fetch(`${API_BASE_URL}/api/lojas/${lojaId.id}`);
+    const loja = await responseLoja.json()
+   
+
+    renderImagePreview(loja.logo_url, loja.banner_url)
     
+}
+
+
+
+function renderImagePreview(logo, banner) {
+
+    const logoImage = document.querySelector(".logo-media")
+    const bannerImage = document.querySelector(".banner-media")
+
+    if (logoImage) {
+            logoImage.src= logo
+   
+    }
+
+    if (bannerImage) {
+            bannerImage.src = banner
+            
+    }
 }
