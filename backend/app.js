111a3308f163f4b0836207892906c53a6f9725d5
Matheus <matheusasantos0501@gmail.com>
2026-07-31 12:50:31 -0300

    test: ajuste no cors

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express()
 
 app.use(cors({
   origin: [
-    "https://guide-app-sigma.vercel.app",
+    "https://guideapp.onrender.com",
     "http://127.0.0.1:5500",
     "http://localhost:5500"
   ]
